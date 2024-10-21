f2a335ef8e6ad41029a075f66d77a433d986caba
Jonathan E. Moussa <40802025+godotalgorithm@users.noreply.github.com>
2024-10-21 16:51:23 -0400

    Update launcher.py to use a stub for MDI options

--- tests/formic/launcher.py
+++ tests/formic/launcher.py
@@ -2,20 +2,8 @@
 import mdi
 import sys
 
-iarg = 1
-while iarg < len(sys.argv):
-    arg = sys.argv[iarg]
-
-    if arg == "-mdi":
-        # Initialize MDI
-        if len(sys.argv) <= iarg+1:
-            raise Exception("Argument to -mdi option not found")
-        mdi.MDI_Init(sys.argv[iarg+1])
-        iarg += 1
-    else:
-        raise Exception("Unrecognized argument")
-
-    iarg += 1
+# Initiate MDI 
+mdi.MDI_Init("-mdi")
 
 # Open plugin
 mdi.MDI_Launch_plugin("mopac", "formic_acid3 -mdi \"-role ENGINE -name mopac -method TCP -hostname localhost -port 8021\"", None, None, None)
