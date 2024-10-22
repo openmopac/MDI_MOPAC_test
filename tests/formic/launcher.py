575b4105890c771aa336f2e7f4736a5cbe90f857
Jonathan E. Moussa <40802025+godotalgorithm@users.noreply.github.com>
2024-10-22 00:38:13 -0400

    Update launcher.py to specify plugin_path

--- tests/formic/launcher.py
+++ tests/formic/launcher.py
@@ -6,4 +6,4 @@ import sys
 mdi.MDI_Init("-mdi")
 
 # Open plugin
-mdi.MDI_Launch_plugin("mopac", "formic_acid3 -mdi \"-role ENGINE -name mopac -method LINK\"", None, None, None)
+mdi.MDI_Launch_plugin("mopac", "formic_acid3 -mdi \"-role ENGINE -name mopac -method LINK -plugin_path /repo/build/mopac/build\"", None, None, None)
