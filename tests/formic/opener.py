98f861accb7a69cf8837ee2ccce026aefa702a96
Jonathan E. Moussa <40802025+godotalgorithm@users.noreply.github.com>
2024-10-22 00:37:44 -0400

    Update opener.py to specify plugin_path

--- tests/formic/opener.py
+++ tests/formic/opener.py
@@ -18,7 +18,7 @@ while iarg < len(sys.argv):
     iarg += 1
 
 # Open plugin
-comm = mdi.MDI_Open_plugin("mopac", "formic_acid3 -mdi \"-role ENGINE -name mopac -method LINK\"", None)
+comm = mdi.MDI_Open_plugin("mopac", "formic_acid3 -mdi \"-role ENGINE -name mopac -method LINK -plugin_path /repo/build/mopac/build\"", None)
 print(f"Communicator: {comm}")
 
 # Get the name of the engine, which will be checked and verified at the end
